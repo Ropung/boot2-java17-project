5f9a271346df21fb5905fb3ddcd3d4de0c3bbc10
Ropung <563710@naver.com>
2023-03-02 00:53:50 +0900

    feat(ddl): create table member

---
+++ src/main/resources/db/migration/R__add_dummy_user.sql
@@ -0,0 +1,10 @@
+INSERT INTO member (
+    username,
+    password,
+    email
+)
+VALUES (
+    'abc123',
+    'abc123!@',
+    'abc123@abc.com'
+);
---
+++ src/main/resources/db/migration/V1_0_1__add_tb_member.sql
@@ -0,0 +1,14 @@
+CREATE TABLE member
+(
+	-- serial 주면 시퀀스 NEXTVAL 자동으로 적용
+	id 				SERIAL,
+	username 		VARCHAR(50) 		UNIQUE 				NOT NULL,
+	password 		VARCHAR(50) 							NOT NULL,
+	email 			VARCHAR(355) 		UNIQUE 				NOT NULL,
+	created_at 		TIMESTAMP 								NOT NULL 			DEFAULT now(),
+	last_login 		TIMESTAMP,
+	
+	CONSTRAINT		pk_member_id 		PRIMARY KEY(id)
+);
+
+CREATE UNIQUE INDEX udx_member_username ON member(username);
