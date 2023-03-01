82d5fb94290440f364f1c3071b61b5591f3a8540
Ropung <563710@naver.com>
2023-03-01 21:24:40 +0900

    initial commit

---
+++ src/main/java/com/ses/projectset/ProjectsetApplication.java
@@ -0,0 +1,13 @@
+package com.ses.projectset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ses/projectset/ProjectsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ses.projectset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
