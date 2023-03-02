6955c160766ad15d4edf6636d81fe89ccebe59a8
Ropung <563710@naver.com>
2023-03-03 01:01:05 +0900

    feat(member): add validation annotation on sign up dto

--- src/main/java/com/ses/projectset/api/dto/MemberRequestDto.java
+++ src/main/java/com/ses/projectset/api/dto/MemberRequestDto.java
@@ -1,9 +1,18 @@
 package com.ses.projectset.api.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
 // record: 불변성.
 public record MemberRequestDto() {
     // inner class
-    public record SignUpRequestDto(String username, String password, String email) {
+    public record SignUpRequestDto(
+        @Length(min=3, max=5) String username,
+        @Pattern(regexp = "^(?=.*[a-zA-Z])((?=.*\\d)(?=.*\\W)).{8,100}$") String password,
+        @NotBlank String email
+    ) {
     }
     
     public record SignInRequestDto(String username, String password) {
